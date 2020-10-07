a3e3715910e000d99a8230eb73ec8c32d3a9568f
Steven Edwards <stevenedwards@MacBook-Pro.local>
2020-10-06 23:50:46 -0400

    updated

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 let count = 0;
 document.querySelector('#count').innerHTML = count; 
 
-let thatForm = document.querySelector('#thisForm').innerHTML;
+function onload() {
+    console.log(count);
+};  
+
+let thatForm = document.getElementById('thisForm'); 
+let bForm = thatForm.value;
 
 let thisPlus = document.querySelector('#plus');
 let thisMinus = document.querySelector('#minus'); 
@@ -9,24 +14,25 @@ let thisMinus = document.querySelector('#minus');
 
 const increaseNumber = () => {
     console.log(count);
-    console.log(thatForm);
-    thatForm += count;  
-    document.querySelector('#count').innerHTML = count;  
+    console.log(bForm);
+    bForm += count;  
+    //document.querySelector('#count').innerHTML = count;  
 }; 
 
 
 const decreaseNumber = () => {
     console.log(count);
-    thatForm -= count;
-    document.querySelector('#count').innerHTML = count; 
+    bForm -= count;
+    //document.querySelector('#count').innerHTML = count; 
 };  
 
 /*const changeColor = () => {
     if (count < 0) {
         document.querySelector('#count').classList.add('color');
     }
+    <form id="aForm" action="get">
 }*/
 
 //event listeners-------------------
 thisPlus.addEventListener('click', increaseNumber);
-thisMinus.addEventListener('click', decreaseNumber);
+thisMinus.addEventListener('click', decreaseNumber); 
